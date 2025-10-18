e415d5bc4591c0a16271ebb45116378cc4687f06
MihailJP <mihailjp@gmail.com>
2025-10-18 15:20:44 +0900

    Translate variable fonts recipe

--- sources/build.sh
+++ sources/build.sh
@@ -13,3 +13,40 @@ font.generate("../fonts/webfonts/${i%sfd}woff2")
 font.close()
 EOS
 done
+
+../scripts/interpolate.py ../build/Inconsolata-LGC-Minimum.sfd Inconsolata-LGC.sfd Inconsolata-LGC-Bold.sfd -1.6
+../scripts/interpolate.py ../build/Inconsolata-LGC-MinimumItalic.sfd Inconsolata-LGC-Italic.sfd Inconsolata-LGC-BoldItalic.sfd -1.6
+../scripts/interpolate.py ../build/Inconsolata-LGC-Maximum.sfd Inconsolata-LGC.sfd Inconsolata-LGC-Bold.sfd 2.5
+../scripts/interpolate.py ../build/Inconsolata-LGC-MaximumItalic.sfd Inconsolata-LGC-Italic.sfd Inconsolata-LGC-BoldItalic.sfd 2.5
+
+pushd ../build
+
+for i in *.sfd; do
+        python3 <<EOS
+import fontforge
+font = fontforge.open("$i")
+font.generate("../build/${i%sfd}ufo")
+font.close()
+EOS
+done
+
+for i in *.ufo; do
+srcdir=.
+stat ${i%ufo}sfd > /dev/null || srcdir=../sources
+sed -i~ -f - <<EOS $i/fontinfo.plist
+/<key>openTypeNameVersion<\/key>/ {
+n
+s/<string>.*<\/string>/\<string>$(grep "^Version: " $srcdir/${i%ufo}sfd | sed -e "s/^Version: //")<\/string>\n\
+    <key>postscriptIsFixedPitch<\/key>\n\
+    <true\/>/
+}
+EOS
+done
+
+../scripts/make_designspace.py Inconsolata-LGC.designspace Inconsolata-LGC.ufo Inconsolata-LGC-Bold.ufo Inconsolata-LGC-Minimum.ufo Inconsolata-LGC-Maximum.ufo
+../scripts/make_designspace.py Inconsolata-LGC-Italic.designspace Inconsolata-LGC-Italic.ufo Inconsolata-LGC-BoldItalic.ufo Inconsolata-LGC-MinimumItalic.ufo Inconsolata-LGC-MaximumItalic.ufo
+
+fontmake -m Inconsolata-LGC.designspace -o variable --output-path ../fonts/variable/Inconsolata-LGC\[wght\].ttf
+fontmake -m Inconsolata-LGC-Italic.designspace -o variable --output-path ../fonts/variable/Inconsolata-LGC-Italic\[wght\].ttf
+
+popd
