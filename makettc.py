86ec6ef4a118fb80903516ff70edd015a80808b5
MihailJP <mihailjp@gmail.com>
2025-11-10 14:00:55 +0900

    Drop Mac Names from TTC

--- makettc.py
+++ makettc.py
@@ -2,4 +2,4 @@
 from sys import argv
 font = fontforge.open(argv[2])
 fonts = [fontforge.open(filename) for filename in argv[3:len(argv)]]
-font.generateTtc(argv[1], fonts, layer=1)
+font.generateTtc(argv[1], fonts, layer=1, flags=('opentype', 'no-mac-names'))
