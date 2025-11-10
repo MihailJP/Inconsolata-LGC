ac52f0a6ae4482c723690ede9244a15f11d0b14e
MihailJP <mihailjp@gmail.com>
2025-11-10 22:48:55 +0900

    Fix TTC (keep OpenType features and really drop Mac names from TTC)

--- makettc.py
+++ makettc.py
@@ -2,4 +2,5 @@
 from sys import argv
 font = fontforge.open(argv[2])
 fonts = [fontforge.open(filename) for filename in argv[3:len(argv)]]
-font.generateTtc(argv[1], fonts, layer=1, flags=('opentype', 'no-mac-names'))
+font.generate(argv[1], flags=('opentype','no-mac-names')) # workaround
+font.generateTtc(argv[1], fonts, layer=1, ttcflags=('merge',))
