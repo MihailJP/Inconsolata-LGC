9bc698ca51e7a32358ad51700be8ef48bd5a482d
MihailJP <mihailjp@gmail.com>
2026-03-23 23:27:20 +0900

    Interpolate anchors too, adjust weight

--- interpolate.py
+++ interpolate.py
@@ -2,9 +2,14 @@
 
 from sys import argv
 import fontforge
+from math import fma
+
+def linear(a: float, b: float, p: float):
+    return fma(b - a, p, a)
 
 font1 = fontforge.open(argv[2])
-font = font1.interpolateFonts(float(argv[4]), argv[3])
+interpolation = float(argv[4])
+font = font1.interpolateFonts(interpolation, argv[3])
 font.encoding = "UnicodeBmp"
 
 font.upos = font1.upos
@@ -39,5 +44,42 @@ font.os2_supyoff = font1.os2_supyoff
 font.os2_supysize = font1.os2_supysize
 
 font.importLookups(font1, font1.gsub_lookups)
+
+if font1.gpos_lookups:
+    font2 = fontforge.open(argv[3])
+    font.importLookups(font1, font1.gpos_lookups)
+    for glyph in font.glyphs():
+        newAnchors = []
+        if glyph.glyphname in font1 and glyph.glyphname in font2:
+            anchors1 = font1[glyph.glyphname].anchorPoints
+            anchors2 = font2[glyph.glyphname].anchorPoints
+            for anchor in glyph.anchorPoints:
+                anchor1 = [a for a in anchors1 if a[0:2] == anchor[0:2]]
+                anchor2 = [a for a in anchors2 if a[0:2] == anchor[0:2]]
+                if anchor[1] in ['base', 'basemark', 'mark']:
+                    if anchor1 and anchor2:
+                        anchor1 = anchor1[0]
+                        anchor2 = anchor2[0]
+                        newAnchors.append((
+                            anchor[0],
+                            anchor[1],
+                            linear(anchor1[2], anchor2[2], interpolation),
+                            linear(anchor1[3], anchor2[3], interpolation),
+                        ))
+                elif anchor[1] == 'ligature':
+                    if anchor1 and anchor2:
+                        anchor1 = [a for a in anchor1 if a[4] == anchor[4]]
+                        anchor2 = [a for a in anchor2 if a[4] == anchor[4]]
+                        if anchor1 and anchor2:
+                            anchor1 = anchor1[0]
+                            anchor2 = anchor2[0]
+                            newAnchors.append((
+                                anchor[0],
+                                anchor[1],
+                                linear(anchor1[2], anchor2[2], interpolation),
+                                linear(anchor1[3], anchor2[3], interpolation),
+                                anchor[4],
+                            ))
+        glyph.anchorPoints = tuple(newAnchors)
 
 font.save(argv[1])
